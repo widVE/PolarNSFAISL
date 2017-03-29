fba46de9876591d87525e634277e485081288640
Andrew Chase <chase3@wisc.edu>
2017-03-29 18:51:29 -0500

    Continued work on EventPanelManager

--- Assets/Scripts/Andrew's Scripts/EventCanvasManager.cs
+++ Assets/Scripts/Andrew's Scripts/EventCanvasManager.cs
@@ -5,11 +5,13 @@ using UnityEngine.UI;
 
 public class EventCanvasManager : MonoBehaviour {
 
+	[SerializeField]
+	private GameObject panel;
 	private Text mytext;
 	private int numEvents = 0;
 	// Use this for initialization
 	void Start () {
-		mytext = this.transform.Find ("Text").GetComponent<Text> ();
+		
 	}
 	
 	// Update is called once per frame
@@ -23,7 +25,15 @@ public class EventCanvasManager : MonoBehaviour {
 			return;
 		}
 		numEvents++;
-		mytext.text += info + " " + numEvents + "\n\n";
+		GameObject newPanel = Instantiate (panel, this.transform);
+		newPanel.name = "EventBox " + numEvents; 
+
+		Vector2 pos = new Vector2 (0f, -150 * (numEvents - 1) + 1025f);
+		Debug.Log ("Position " + numEvents + ": " + pos);
+		newPanel.GetComponent<Image> ().color = Random.ColorHSV();
+		newPanel.GetComponent<RectTransform> ().anchoredPosition = pos;
+
+		newPanel.transform.Find ("Text").GetComponent<Text> ().text = "Event " + numEvents;
 
 	}
 }

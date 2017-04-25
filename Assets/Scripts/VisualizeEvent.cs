93245ba33761ce8eddae72d158d0d377c0ab8f40
Ross Tredinnick <ross.tredinnick@gmail.com>
2017-04-24 20:32:31 -0500

    Checking in work in progress on speeding up / slowing down play back and parsing of new event files.

--- Assets/Scripts/VisualizeEvent.cs
+++ Assets/Scripts/VisualizeEvent.cs
@@ -7,12 +7,14 @@ using System.Collections.Generic;
 public class VisualizeEvent : MonoBehaviour {
 
     public string eventDirectory;
+    public string newEventFile;
+
     //public GameObject particle;
     public float playSpeed = 0.01f;
     public float eventFrequency = 15.0f;
     public GameObject totalEnergyText = null;
 
-    private float totalEnergy = 0.0f;
+    public float totalEnergy = 0.0f;
     
     private const float BELOW_ICE = -1950.0f;
     private float lastPlayTime = 0.0f;
@@ -129,22 +131,83 @@ public class VisualizeEvent : MonoBehaviour {
                         e.eventSource = sources[UnityEngine.Random.Range(0, numSources - 1)];
                     }
                     events.Add(e);
+                    
                 }
             }
+        }
 
-            if(events.Count > 0)
+        if(newEventFile.Length > 0)
+        {
+            StreamReader sr = new StreamReader(newEventFile, Encoding.Default);
+            string s = sr.ReadLine();
+
+            bool first = true;
+
+            GameObject[] sources = GameObject.FindGameObjectsWithTag("NeutrinoSource");
+            int numSources = sources.Length;
+            List<EventData> ed = new List<EventData>();
+            while (s != null)
             {
-                eventsPlaying = new EventPlayback[events.Count];
-                for(int e = 0; e < events.Count; ++e)
+                if(s[0] != '#')
                 {
-                    eventsPlaying[e].isPlaying = false;
-                    eventsPlaying[e].eventIndex = -1;
-                    eventsPlaying[e].eventStartTime = 0.0f;
-                    eventsPlaying[e].eventEndTime = 0.0f;
-                    eventsPlaying[e].playStartTime = 0.0f;
-                    eventsPlaying[e].advancedIndex = false;
-                    eventsPlaying[e].newPlayTime = 0.0f;
+                    string[] data = s.Split(' ');
+                    if(data.Length == 2)
+                    {
+                        //theta, phi
+                    }
+                    else if(data.Length == 7)
+                    { 
+                        //string, dom, x, y, z, time, charge
+                        EventData d;
+                        d.dom = IntParseFast(data[0]) - 1;
+                        d.str = IntParseFast(data[1]) - 1;
+                        d.pos.x = (float)double.Parse(data[2]);
+                        d.pos.y = BELOW_ICE + (float)double.Parse(data[4]);
+                        d.pos.z = (float)double.Parse(data[3]);
+                        d.charge = (float)double.Parse(data[6]);
+                        d.time = (float)double.Parse(data[5]);
+                        ed.Add(d);
+                    }
                 }
+                else
+                {
+                    if(s[1] == 'e')
+                    {
+                        //new event...
+                        //store old event...
+                        if (!first)
+                        {
+                            EventVis e = new EventVis();
+                            e.eventData = new List<EventData>(ed);
+
+                            e.eventData.Sort((s1, s2) => s1.time.CompareTo(s2.time));
+                            if (sources.Length > 0)
+                            {
+                                e.eventSource = sources[UnityEngine.Random.Range(0, numSources - 1)];
+                            }
+                            events.Add(e);
+                            ed.Clear();
+                        }
+                        first = false;
+                    }
+                }
+                s = sr.ReadLine();
+            }
+        }
+
+        if(events.Count > 0)
+        {
+            Debug.Log(events.Count);
+            eventsPlaying = new EventPlayback[events.Count];
+            for(int e = 0; e < events.Count; ++e)
+            {
+                eventsPlaying[e].isPlaying = false;
+                eventsPlaying[e].eventIndex = -1;
+                eventsPlaying[e].eventStartTime = 0.0f;
+                eventsPlaying[e].eventEndTime = 0.0f;
+                eventsPlaying[e].playStartTime = 0.0f;
+                eventsPlaying[e].advancedIndex = false;
+                eventsPlaying[e].newPlayTime = 0.0f;
             }
         }
 	}
@@ -172,11 +235,12 @@ public class VisualizeEvent : MonoBehaviour {
         //r or every 60 seconds
         if (UnityEngine.Input.GetKeyDown(UnityEngine.KeyCode.R) || t - lastPlayTime > eventFrequency)
         {
-            Debug.Log("Playing event!");
+            
 			lastPlayTime = t;
             
 			//todo - don't allow same event to replay until it's done...
             int currEvent = UnityEngine.Random.Range(0, events.Count);
+            Debug.Log("Playing event " + currEvent);
 
             eventsPlaying[currEvent].newPlayTime = t;
             eventsPlaying[currEvent].eventStartTime = events[currEvent].eventData[0].time;
@@ -249,22 +313,21 @@ public class VisualizeEvent : MonoBehaviour {
                         //Debug.Log("Frame Count: " + UnityEngine.Time.frameCount);
                         //Debug.Log("Event Index: " + eventsPlaying[e].eventIndex);
                         //need to double check this...
-                        if (frameFrac < (float)UnityEngine.Time.frameCount)
-                        {
+                        //if (frameFrac < (float)UnityEngine.Time.frameCount)
+                        //{
                             eventsPlaying[e].eventIndex++;
                             eventsPlaying[e].advancedIndex = true;
-                        }
+                        /*}
                         else if(UnityEngine.Time.frameCount > eventsPlaying[e].eventEndFrame)
                         {
                             StopPlaying(e);
-                            //eventsPlaying[e].eventIndex++;
                         }
                         else
                         {
                             //spin the existing spheres?
                             //fade out option?
                             eventsPlaying[e].advancedIndex = false;
-                        }
+                        }*/
                     }
 
                     if (eventsPlaying[e].eventIndex >= events[e].eventData.Count - 1)

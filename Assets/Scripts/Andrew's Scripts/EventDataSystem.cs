c03292d548eb0a14571c16f39f65da98ef234892
Andrew Chase <chase3@wisc.edu>
2017-05-30 14:40:50 -0500

    Developing network message system, currently debugging

--- Assets/Scripts/Andrew's Scripts/EventDataSystem.cs
+++ Assets/Scripts/Andrew's Scripts/EventDataSystem.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class EventDataSystem : MonoBehaviour {
+public class EventDataSystem : NetworkBehaviour {
 
-	NetworkClient myClient;
-	[SerializeField]
-	private int port;
+	public NetworkClient myClient;
+
+	private int port = 5555;
 
 	public class MyMessageTypes {
-		public static short EventDataID = MsgType.Highest + 1;
-		public static short EventRequestID = (short)(EventDataID + 1);
+		public static short EventRequestID = MsgType.Highest + 1;
+		public static short EventDataID = (short)(EventRequestID + 1);
+		public static short EventResultID = (short)(EventDataID + 1);
 	};
 
 	/// <summary>
@@ -20,7 +21,7 @@ public class EventDataSystem : MonoBehaviour {
 	public class EventDataMessage : MessageBase{
 		public Vector2 coordinates;
 		public float energy;
-		public string source;
+		public int destinationClientID;
 	};
 
 
@@ -28,41 +29,167 @@ public class EventDataSystem : MonoBehaviour {
 	/// Message used to request event data from the table top
 	/// </summary>
 	public class EventRequestMessage : MessageBase{
-		public string name;
+		public int sourceClientID;
 	};
 
+	public class EventResultMessage : MessageBase{
+		public string phenomenaName;
+		public int sourceClientID;
+	};
 
-	public void SendData(Vector2 coordinates, float energy, string source, int clientID) {
+	/// <summary>
+	/// Sends event data - should only be called from the tabletop!
+	/// </summary>
+	/// <param name="coordinates">Coordinates.</param>
+	/// <param name="energy">Energy.</param>
+	/// <param name="source">Source.</param>
+	/*public void SendData(Vector2 coordinates, float energy, string source) {
+
+		if (!isServer) {
+			Debug.LogError ("SendData was called from non-host client - did you mean to use SendResults?");
+		}
+
 		EventDataMessage msg = new EventDataMessage ();
 		msg.source = source;
 		msg.energy = energy;
 		msg.coordinates = coordinates;
+		msg.clientID = -1;
 
-		NetworkServer.SendToClient (clientID, MyMessageTypes.EventDataID, msg);
+		myClient.Send (MyMessageTypes.EventDataID, msg);
+
+		//NetworkServer.SendToClient (clientID, MyMessageTypes.EventDataID, msg);
+	}*/
+
+	/// <summary>
+	/// Sends a request for data to the tabletop - ONLY Oculus clients should call this!
+	/// </summary>
+	/// <param name="name">Name.</param>
+	public void SendRequest() {
+		EventRequestMessage msg = new EventRequestMessage ();
+		msg.sourceClientID = -1;
+
+		myClient.Send (MyMessageTypes.EventRequestID, msg);
+	}
+
+	public void SendResults(string resultName) {
+		EventResultMessage msg = new EventResultMessage ();
+		msg.phenomenaName = name;
+		msg.sourceClientID = -1;
+
+		myClient.Send (MyMessageTypes.EventResultID, msg);
 	}
 
 	public void SetupClient() {
 		myClient = new NetworkClient ();
 		myClient.RegisterHandler (MsgType.Connect, OnConnected);
-		myClient.RegisterHandler (MyMessageTypes.EventDataID, OnDataReceived);
-		myClient.RegisterHandler (MyMessageTypes.EventRequestID, OnRequestReceived);
+		myClient.RegisterHandler (MyMessageTypes.EventDataID, ClientDataReceived);
+		myClient.RegisterHandler (MyMessageTypes.EventRequestID, ClientRequestReceived);
+		myClient.RegisterHandler (MyMessageTypes.EventResultID, ClientResultReceived);
 		myClient.Connect ("127.0.0.1", port);
+	}
+
+	public void SetupServer() {
+		NetworkServer.RegisterHandler (EventDataSystem.MyMessageTypes.EventRequestID, ServerHandleRequest);
+		NetworkServer.RegisterHandler (EventDataSystem.MyMessageTypes.EventDataID, ServerHandleData);
+		NetworkServer.RegisterHandler (EventDataSystem.MyMessageTypes.EventResultID, ServerHandleRequest);
 	}
 
 	public void OnConnected(NetworkMessage netMsg) {
 		Debug.Log("Connected to server");
 	}
 
-	public void OnDataReceived(NetworkMessage netMsg) {
-		EventDataMessage msg = netMsg.ReadMessage<EventDataMessage> ();
+	/// <summary>
+	/// Handler for when a request is received - should only be triggered on the tabletop.
+	/// </summary>
+	/// <param name="netMsg">Net message.</param>
+	public void ClientRequestReceived(NetworkMessage netMsg) {
 
-		// Do stuff with the data now
-		Debug.Log("Data Message received: " + msg.source);
-	}
 
-	public void OnRequestReceived(NetworkMessage netMsg) {
+		if (!isServer) {
+			Debug.LogError ("Request received by non-host client");
+		}
 		EventRequestMessage msg = netMsg.ReadMessage<EventRequestMessage> ();
+		Debug.Log("Client received request");
+
+		// Temporary - send blank message back to client
+		EventDataMessage response = new EventDataMessage();
+		response.coordinates = new Vector2 (0f, 0f);
+		response.energy = 10f;
+		response.destinationClientID = msg.sourceClientID;
+
+		myClient.Send (MyMessageTypes.EventDataID, response);
+
+		// NetworkServer.SendToClient (netMsg.conn.connectionId, MyMessageTypes.EventDataID, response);
 		// Do stuff with this request, create a new EventDataMessage, and send it back to
 		// whomever sent the request
+
+		// Access data from our list
+
+		// Make a response message
+
+
+		// Send it to the client that asked for it
+	}
+
+	public void ClientDataReceived(NetworkMessage netMsg) {
+
+		EventDataMessage msg = netMsg.ReadMessage<EventDataMessage> ();
+
+		Debug.Log ("Client received data message");
+
+		// Do stuff with the message, like look at it
+
+		// TESTING ONLY - immediately send a response
+		SendResults("Good data");
+	}
+
+	public void ClientResultReceived(NetworkMessage netMsg) {
+
+		EventResultMessage msg = netMsg.ReadMessage<EventResultMessage> ();
+
+		if (isServer) {
+			Debug.Log ("Storing results in our list - Hurray client " + msg.sourceClientID + " found a " + msg.phenomenaName + "!");
+		} else {
+			Debug.Log("Hurray client " + msg.sourceClientID + " found a " + msg.phenomenaName + "!");
+		}
+
+	}
+
+
+
+
+	//------------------------SERVER SIDE-----------------------------------
+
+
+	// Forward to host
+	void ServerHandleRequest(NetworkMessage netMsg) {
+
+		Debug.Log ("Server received request");
+		EventRequestMessage msg = netMsg.ReadMessage<EventRequestMessage> ();
+		msg.sourceClientID = netMsg.conn.connectionId;
+
+		// Send this to host
+		NetworkServer.SendToClient(NetworkServer.serverHostId, MyMessageTypes.EventRequestID, msg);
+		Debug.Log ("Server sent request to client id: " + NetworkServer.serverHostId);
+		Debug.Log ("Number of server connections: " + NetworkServer.connections.Count);
+	}
+
+	// Forward to client
+	void ServerHandleData(NetworkMessage netMsg) {
+
+		Debug.Log ("Server received data");
+		EventDataMessage msg = netMsg.ReadMessage<EventDataMessage> ();
+
+		NetworkServer.SendToClient (msg.destinationClientID, MyMessageTypes.EventDataID, msg);
+
+	}
+
+	void ServerHandleResult(NetworkMessage netMsg) {
+
+		Debug.Log ("Server received result");
+		EventResultMessage msg = netMsg.ReadMessage<EventResultMessage> ();
+		msg.sourceClientID = netMsg.conn.connectionId;
+
+		NetworkServer.SendToAll (MyMessageTypes.EventResultID, msg);
 	}
 }
---
+++ Assets/Scripts/Andrew's Scripts/MessageCreator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+/// <summary>
+/// FOR TESTING MESSAGE SYSTEM ONLY - NOT FOR USE IN FINAL PROJECT
+/// </summary>
+public class MessageCreator : NetworkBehaviour {
+
+	EventDataSystem system;
+	float timer = 5f;
+
+	// Use this for initialization
+	void Start () {
+		system = new EventDataSystem ();
+		system.SetupClient ();
+
+		if (isServer) {
+			system.SetupServer ();
+			Debug.Log ("Server set up");
+			Debug.Log ("Number of server connections: " + NetworkServer.connections.Count);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Don't send requests from the table top
+		if (isServer) {
+			return;
+		}
+
+		timer -= Time.deltaTime;
+
+		if (timer <= 0f) {
+			timer = 5f;
+
+			system.SendRequest ();
+			Debug.Log ("Client sent request message to server");
+
+		}
+	}
+}

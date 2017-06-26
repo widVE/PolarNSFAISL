979b721d26369b6cd984ab92416c31f349f8f838
Andrew Chase <chase3@wisc.edu>
2017-06-26 16:01:49 -0500

    Added sliders for puzzle camera movement and dom manipulation

--- Assets/Scripts/Andrew's Scripts/PuzzleCameraController.cs
+++ Assets/Scripts/Andrew's Scripts/PuzzleCameraController.cs
@@ -1,17 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PuzzleCameraController : MonoBehaviour {
 
+	private Vector3 currentTarget;
 	private bool isMoving = false;
 	private Vector3 destPos;
 	private Quaternion facingDir;
+	[SerializeField]
+	private GameObject sliders;
+
+	private Slider sizeSlider;
+	private Slider rotateSlider;
+	private Slider zoomSlider;
+
+	private float prevSliderValue;
+	private float currDegree = 0f;
+
 
 	private List<VisualizeEvent.DomSnapShot> prevSnapShots;
 
 	void Start() {
 		facingDir = Quaternion.LookRotation(new Vector3(0,0,1f));
+		if (sliders == null) {
+			Debug.LogError ("No reference to the sliders");
+		}
+		sizeSlider = sliders.transform.Find ("DomSizeSlider").GetComponent<Slider> ();
+		rotateSlider = sliders.transform.Find ("RotateSlider").GetComponent<Slider> ();
+		zoomSlider = sliders.transform.Find ("ZoomSlider").GetComponent<Slider> ();
+
+		prevSliderValue = sizeSlider.value;
 	}
 
 
@@ -33,14 +53,27 @@ public class PuzzleCameraController : MonoBehaviour {
 					curr.Dom.GetComponent<DOMController> ().TurnOn (curr.timeFrac, curr.charge);
 				}
 			}
+
+			if (!isMoving) {
+				AdjustCamera ();
+			}
+
+		}
+
+
+		if (prevSliderValue != sizeSlider.value) {
+			prevSliderValue = sizeSlider.value;
+			ChangeDomScale ();
 		}
 
 	}
 
 	public void MoveCamera(Vector3 targetPos, List<VisualizeEvent.DomSnapShot> snapShots) {
-
+		currentTarget = targetPos;
+		ResetSliders ();
 		if (prevSnapShots != null) {
 			foreach (VisualizeEvent.DomSnapShot curr in prevSnapShots) {
+				//curr.Dom.transform.localScale = new Vector3 (1f, 1f, 1f);
 				curr.Dom.GetComponent<DOMController> ().TurnOff ();
 			}
 		}
@@ -61,13 +94,42 @@ public class PuzzleCameraController : MonoBehaviour {
 	public void CleanUp() {
 		if (prevSnapShots != null) {
 			foreach (VisualizeEvent.DomSnapShot curr in prevSnapShots) {
+				//curr.Dom.transform.localScale = new Vector3 (1f, 1f, 1f);
 				curr.Dom.GetComponent<DOMController> ().TurnOff ();
 			}
+			ResetSliders ();
 		}
 
 		prevSnapShots = null;
-
 		MoveCamera (new Vector3 (3000, -1000, -2000), null);
 
+	}
+
+	private void ChangeDomScale() {
+		foreach (VisualizeEvent.DomSnapShot curr in prevSnapShots) {
+			curr.Dom.transform.localScale = (new Vector3 (1f, 1f, 1f) * sizeSlider.value); 
+		}
+	}
+
+	private void AdjustCamera() {
+		float rotateValue = rotateSlider.value;
+		float currDistance = Vector3.Distance (this.transform.position, currentTarget);
+		float diffToMove = currDistance - zoomSlider.value;
+
+		float rotateDiff = rotateValue - currDegree;
+		transform.RotateAround (currentTarget, Vector3.up, rotateDiff);
+		transform.LookAt (currentTarget);
+		currDegree = rotateValue;
+
+
+		transform.Translate (this.transform.forward * diffToMove, Space.World);
+
+		currDistance = diffToMove;
+	}
+
+	private void ResetSliders() {
+		sizeSlider.value = 1.0f;
+		rotateSlider.value = 0f;
+		zoomSlider.value = 1000f;
 	}
 }

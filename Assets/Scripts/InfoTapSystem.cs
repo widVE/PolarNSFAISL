fa6ccb1bad0cf3b040da06a907fa49440d4a4db7
Ross Tredinnick <ross.tredinnick@gmail.com>
2017-08-15 12:01:31 -0500

    Added a place holder system for double tapping to show further info over objects. Positioned a possible console scroll view.

---
+++ Assets/Scripts/InfoTapSystem.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TouchScript.Gestures;
+
+public class InfoTapSystem : MonoBehaviour
+{
+    private const int MAX_INFO_MSGS = 5;
+    struct InfoBounds
+    {
+        //bounds in viewport space
+        public Vector2 minBounds;
+        public Vector2 maxBounds;
+        public string infoMsg;
+    };
+
+    InfoBounds[] infoArray = new InfoBounds[MAX_INFO_MSGS];
+    // Use this for initialization
+    void Start()
+    {
+        infoArray[0].minBounds.Set(0.415625f, 0.6f);
+        infoArray[0].maxBounds.Set(0.58f, 0.9f);
+        infoArray[0].infoMsg = "You tapped the ICL";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnEnable()
+    {
+        GetComponent<TapGesture>().Tapped += tappedHandler;
+    }
+
+    private void OnDisable()
+    {
+        GetComponent<TapGesture>().Tapped -= tappedHandler;
+    }
+
+    private void tappedHandler(object sender, System.EventArgs e)
+    {
+        Debug.Log("double tap");
+        Vector2 screenPos = GetComponent<TapGesture>().NormalizedScreenPosition;
+        for(int i = 0; i < MAX_INFO_MSGS; ++i)
+        {
+            if(screenPos.x > infoArray[i].minBounds.x && screenPos.x < infoArray[i].maxBounds.x &&
+                screenPos.y > infoArray[i].minBounds.y && screenPos.y < infoArray[i].maxBounds.y)
+            {
+                //show the string on screen in some sort of UI bubble..
+                Debug.Log(infoArray[i].infoMsg);
+            }
+        }
+    }
+}

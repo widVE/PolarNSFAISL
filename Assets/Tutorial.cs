36730ec1abff13a6c3d6feccf0fcb686330c77b8
Ross Tredinnick <ross.tredinnick@gmail.com>
2017-08-22 11:17:52 -0500

    missing tutorials

---
+++ Assets/Tutorial.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour {
+
+    private int currPanelIndex = 0;
+    private float lastTime = 0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float currTime = Time.time;
+        if (currTime - lastTime > 10f)
+        {
+            lastTime = currTime;
+            int nextPanelIndex = currPanelIndex+1;
+            if(nextPanelIndex == transform.childCount)
+            {
+                nextPanelIndex = 0;
+            }
+
+            transform.GetChild(currPanelIndex).gameObject.SetActive(false);
+            transform.GetChild(nextPanelIndex).gameObject.SetActive(true);
+
+            currPanelIndex = nextPanelIndex;
+        }
+	}
+}

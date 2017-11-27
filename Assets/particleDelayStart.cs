5c7d8bad940c7aa4ba65f2ace249a75b7f579337
Brady Boettcher <bboettcher2@wisc.edu>
2017-11-27 11:27:39 -0600

    added particle effect delay and duration

---
+++ Assets/particleDelayStart.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particleDelayStart : MonoBehaviour {
+    ParticleSystem system;
+
+	// Use this for initialization
+	void Start () {
+        system = gameObject.transform.GetChild(2).GetComponent<ParticleSystem>();
+        system.Stop();
+        StartCoroutine(Wait(1.5f));
+        //system.Play();
+
+        Debug.Log(gameObject.transform.GetChild(2).name);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private IEnumerator Wait(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        system.Play();
+    }
+}

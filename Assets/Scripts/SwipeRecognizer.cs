1bcf548d81875e7f056f12a1cd9631257894b219
malulleybovo <malulleybovo@wisc.edu>
2017-11-30 19:10:45 -0600

    Added Sphere Map UI and Functionality
    - Every detected event has its origin location on the galaxy recorded by a red dot on the sphere map UI.

--- Assets/Scripts/SwipeRecognizer.cs
+++ Assets/Scripts/SwipeRecognizer.cs
@@ -738,9 +738,9 @@ public class SwipeRecognizer : MonoBehaviour {
                             {
                                 Vector3 dir = currentEvents.events[currentEvents.lastEventNumber].startPos - currentEvents.events[currentEvents.lastEventNumber].endPos;
                                 dir = dir.normalized;
-                                float longitude = Mathf.Acos(Vector3.Dot(dir, Vector3.up));
-                                float lat = Mathf.Acos(Vector3.Dot(dir, Vector3.forward));
-                                //sphereMap.PlotPoint(new Vector2(lat, longitude));
+                                float longitude = (2 * Mathf.Acos(Vector3.Dot(dir, Vector3.up)) / Mathf.PI) - 1;
+                                float lat = (2 * Mathf.Acos(Vector3.Dot(dir, Vector3.forward)) / Mathf.PI) - 1;
+                                sphereMap.PlotPoint(lat, longitude);
                             }
 
                             /*if(scorePanel != null)

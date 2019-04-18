68d3b60443f3d6361fcf6ef2aac85d2309842606
Mann <smann@Morgridge.net>
2019-04-18 14:46:58 -0500

    More SFX!
    Swish swish swish
    Sound effects are better, BGM is updated for now

---
+++ Assets/sfxController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sfxController : MonoBehaviour
+{
+    public AudioSource glass;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+    public void playGlassClick()
+    {
+        Debug.Log("Click!");
+        glass.Play();
+    }
+}
